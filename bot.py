d51f46d66cbe15c57cd324f6996009b3c37b58d9
monsterr0730 <bgmi83071@gmail.com>
2026-04-21 20:41:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from collections import defaultdict
 from pymongo import MongoClient
 
 # ========== CONFIG ==========
-BOT_TOKEN = "8760406918:AAG_gL-4kn0sO0NfacWpO6PwzhSbIiX-9co"
+BOT_TOKEN = "8760406918:AAEgtuPMVhNMM-A9gtuAu9hjAJVm_lkaURQ"
 ADMIN_ID = ["8487946379"]
 API_URL = "http://cnc.teamc2.xyz:5001/api/attack"
 API_KEY = "WTRMWL"
